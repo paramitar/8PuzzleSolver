22a0134fa9d582d1773cb962bd9804cc51ebe1f0
paramitar <mitaparamitar@gmail.com>
2013-12-18 19:00:06 +0700

    Create Coor.java

---
+++ Coor.java
@@ -0,0 +1,15 @@
+package tubesstrukdatpuzzle;
+public class Coor { 
+	int x;
+	int y;
+
+	public Coor(int x, int y) { 
+		this.x = x;
+		this.y = y;
+	}
+
+	public Coor() { //method
+		this.x = 0;
+		this.y = 0;
+	}
+}
